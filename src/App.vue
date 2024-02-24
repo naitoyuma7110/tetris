215fbfd14d52806cb2b283121343382102a4a113
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-02-25 03:05:51 +0900

    .env作成、VITE_BASE_URL設定通らず

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
-
+const baseUrl = import.meta.env.BASE_URL;
+console.log(baseUrl); // "/my-app/"
 </script>
 
 <template>
-  <router-view />
+  <v-app>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Tetris</v-toolbar-title>
+      <router-link to="/">TopPage</router-link>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+
+    <!-- アプリケーションのコンポーネントに基づいてコンテンツのサイズを決定 -->
+    <v-main>
+      <!-- vue-routerを使用する場合 -->
+      <router-view />
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
 </template>

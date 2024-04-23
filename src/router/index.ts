fae0de2bc2155dbee83b2536acd485354ae5d0d2
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-04-24 00:15:49 +0900

    参照渡しの検証

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import TopPage from '@/pages/TopPage.vue'
 import PlayPage from '@/pages/PlayPage.vue'
 import PiniaTest from '@/pages/PiniaTest.vue'
+import TestPage from '@/pages/TestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +13,11 @@ const router = createRouter({
       path: '/',
       name: 'TopPage',
       component: TopPage
+    },
+    {
+      path: '/test',
+      name: 'TestPage',
+      component: TestPage
     },
     {
       path: '/play',

38c66d01051d6792bcb8f0e52340878d983fdc66
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-04-25 22:04:27 +0900

    縦スクロール非表示にするため画面heightの修正

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,19 @@
     <v-app-bar app>
       <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>Tetris</v-toolbar-title>
-
     </v-app-bar>
     <v-main>
       <router-view />
     </v-main>
   </v-app>
 </template>
+
+<style>
+.v-application__wrap {
+  min-height: 80vh !important;
+}
+
+/* [data-vuetify] .v-application--wrap {
+  min-height: 0vh !important;
+} */
+</style>

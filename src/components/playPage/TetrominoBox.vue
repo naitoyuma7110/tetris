8eeab1edd079f6b7a794dff0c687ce4ff8296ba7
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-05-03 03:25:12 +0900

    分割代入の修正とcomputedによるボックス内でのtailesOnFieldの算出

--- src/components/playPage/TetrominoBox.vue
+++ src/components/playPage/TetrominoBox.vue
@@ -1,30 +1,42 @@
 <script lang="ts" setup>
 import { type Point, type TETROMINO_TYPE } from '@/common/Tetromino';
 import { getBlockClass } from '@/common/utils';
+import { computed, toRefs } from 'vue';
 
-const props = (defineProps(['tetromino']))
 
-console.log(props.tetromino)
+const props = defineProps(['tetromino'])
+const { tetromino } = toRefs(props);
 
-const boxField = [
-  [0, 0, 0],
-  [0, 0, 0],
-  [0, 0, 0]]
 
-props.tetromino.tilePoints.forEach((point: Point) => {
-  boxField[point[0]][point[1]] = props.tetromino.tetrominoType
-})
+const tetrominoField = computed(() => {
+
+  const field = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],]
+
+  if (tetromino?.value) {
+    tetromino.value.tilesOnfield.forEach((point: Point) => {
+      field[point[0]][point[1]] = tetromino.value.tetrominoType
+    })
+  }
+
+  return field
+}
+)
 
 </script>
 
 <template>
   <div>
-    <div class="game-board-row" v-for="(row, y) in boxField" :key="y">
+    <div class="game-board-row" v-for="(row, y) in tetrominoField" :key="y">
       <span class="game-board-col" v-bind:class="getBlockClass(col as (TETROMINO_TYPE))" v-for=" (col, x) in row"
         :key="() => `${x}${y}`">
-        {{ col }}
       </span>
     </div>
   </div>
 </template>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.block-blank {
+  border: none;
+}
+</style>

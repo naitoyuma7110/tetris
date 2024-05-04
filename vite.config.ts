002ac0bb6ba25b20d908d5c59795220e7f27ad04
naitoyuma7110 <naitoyuma1118@gmail.com>
2024-05-05 04:53:13 +0900

    #4 github pagesのURLに含まれるリポジトリ名も考慮したbase設定

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/docs',
+  base: process.env.NODE_ENV === 'production' ? '/tetris/' : './',
   build: {
     outDir: 'docs'
   }
